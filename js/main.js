6d4b0b982216e75c6d34e049bd789d5e011e80b2
freakyvictory <freakyvictory@gmail.com>
2023-05-23 20:25:12 -0500

    plus

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
-$(function(){
-  
-  $('.slider__inner').slick({
-  infinite:true,
-  slidesToShow: 1,
-slidesToScroll: 1,
-  arrows: false,
-  dots: true,
-  autoplay: true,
-  autoplaySpeed: 1200,
-});
-
-
-});
+$(function () {
+  $(".slider__inner").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1600,
+  })
+})
