10ddedab0fc5ced122413297f922c197324c5f5a
freakyvictory <freakyvictory@gmail.com>
2023-05-25 14:58:04 -0500

    ss

--- js/main.js
+++ js/main.js
@@ -9,3 +9,20 @@ $(function () {
     autoplaySpeed: 1600,
   })
 })
+
+$('.works__inner').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.works__inner'
+});
+
+// $('.works__inner').slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   asNavFor: '.works__inner',
+//   dots: true,
+//   centerMode: true,
+//   focusOnSelect: true
+// });
