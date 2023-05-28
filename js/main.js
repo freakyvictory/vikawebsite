94d63e0c3ac2222a26b4bb035bad760897a986dc
freakyvictory <freakyvictory@gmail.com>
2023-05-27 19:38:53 -0500

    save

--- js/main.js
+++ js/main.js
@@ -9,14 +9,24 @@ $(function () {
     autoplaySpeed: 1600,
   })
 })
-
-$('.works__inner').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: '.works__inner'
-});
+$(function () {
+  $(".works__inner").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: ".slider-nav",
+  })
+  $(".slider-nav").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: ".works__inner",
+    dots: true,
+    centerMode: true,
+    slick-dots: '<button class="slick-dots slider-nav"></button>',
+    focusOnSelect: true,
+  })
+})
 
 // $('.works__inner').slick({
 //   slidesToShow: 3,
